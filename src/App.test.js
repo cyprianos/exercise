a3676c5e277c60652a17bedd1d0bcedc3e837798
Cyprian Gepfert <cyprian.gepfert@codete.com>
2018-11-03 16:46:32 +0100

    basic enzyme scenario

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import {shallow} from 'enzyme';
+
 import App from './App';
+
+
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing by using enzyme', ()=> {
+  const wrapper = shallow(<App/>);
+  const welcome = <h2>Welcome to React</h2>;
+  expect(wrapper.contains(welcome)).toBe(true);
+
 });
