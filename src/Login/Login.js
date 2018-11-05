7bb4b90b9dc8b23edbff8248b99786e021e9279c
Cyprian Gepfert <cyprian.gepfert@codete.com>
2018-11-05 04:28:25 +0100

    formik & yup based login form validation

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,23 +1,51 @@
 import React from "react";
-import {Button, Form, FormGroup, Label, Input} from 'reactstrap';
+import {Button, Form, FormGroup, Label, Input, Alert} from 'reactstrap';
+import {Formik, Field, ErrorMessage} from "formik";
+import * as Yup from 'yup';
+
+const LoginFormValidationSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(5, 'Too short username')
+    .required('Required!'),
+  password: Yup.string()
+    .min(8, 'Too short password')
+    .required('Required!')
+});
+
 
 export default class Login extends React.Component {
   render() {
     return (
-      <Form>
-        <h3>Login</h3>
-        <FormGroup>
-          <Label for="username">Username</Label>
-          <Input id="username" name="username" placeholder="Username...."/>
-        </FormGroup>
-        <FormGroup>
-          <Label for="password" >Password</Label>
-          <Input id="password" name="password" type="password" placeholder="Password..."/>
-        </FormGroup>
+      <Formik initialValues={{username: 'x', password: 'z'}}
+              validationSchema={LoginFormValidationSchema}
+              onSubmit={(values, actions) => {
+                console.log(values);
+                // setTimeout(() => {
+                //   alert(JSON.stringify(values, null, 2));
+                //   actions.setSubmitting(false);
+                // }, 400);
+              }}
+      >{({errors, touched, handleSubmit, handleChange}) => (
+        <Form onSubmit={handleSubmit}>
+          <h3>Login</h3>
+          <FormGroup>
+            <Label for="username">Username</Label>
+            <Input onChange={handleChange} id="username" name="username" />
+            <ErrorMessage name="username">{msg => <div>{msg}</div>}</ErrorMessage>
+          </FormGroup>
+          <FormGroup>
+            <Label for="password">Password</Label>
+            <Input onChange={handleChange} id="password" name="password" type="password" />
+            <ErrorMessage name="password">{msg => <div>{msg}</div>}</ErrorMessage>
+          </FormGroup>
 
-        <Button>Submit</Button>
+          <Button type="submit">Submit</Button>
 
-      </Form>
+        </Form>
+      )}
+
+      </Formik>
+
     )
 
   }
