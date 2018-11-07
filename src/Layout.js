41e31f3957afed5143d944c6ae8632a7147501e3
Cyprian Gepfert <cyprian.gepfert@codete.com>
2018-11-08 00:17:19 +0100

    page not found route

--- src/Layout.js
+++ src/Layout.js
@@ -1,15 +1,12 @@
-
 import React from "react";
 
 import {Container} from "reactstrap";
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 import {connect} from "react-redux";
 
 import {modalError} from './Modal/Modal.actions'
 import AuthProvider, {AuthContext} from "./Auth/AuthProvider";
-
-
 
 import Wall from "./Wall/Wall";
 import Login from "./Login/Login";
@@ -17,6 +14,7 @@ import SecretRoute from "./Auth/SecretRoute";
 import Details from "./Details/Details";
 
 import Modal from "./Modal/Modal";
+import NoMatch from "./NoMatch";
 
 class Layout extends React.Component {
   render() {
@@ -34,11 +32,18 @@ class Layout extends React.Component {
               {(auth) => (
                 <Container className="main">
                   <h2>HSBC Exercise</h2>
-                  <button onClick={()=>{ return this.props.modalError('Fake Error!')}}>Make Error!</button>
-                  <Route exact path="/" component={Wall}/>
-                  <Route path="/login" component={Login}/>
-                  <SecretRoute path="/details/:id" component={Details}/>
-                  <SecretRoute path="/wall" component={Wall}/>
+                  <button onClick={() => {
+                    return this.props.modalError('Fake Error!')
+                  }}>Make Error!
+                  </button>
+
+                  <Switch>
+                    <SecretRoute path="/details/:id" component={Details}/>
+                    <SecretRoute path="/wall" component={Wall}/>
+                    <SecretRoute exact path="/" component={Wall}/>
+                    <Route path="/login" component={Login}/>
+                    <Route component={NoMatch}/>
+                  </Switch>
                 </Container>
               )}
 
